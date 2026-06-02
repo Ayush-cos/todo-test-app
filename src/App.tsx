9ecab01b3de507bd2bece10167111be001f5b05e
Copan <copan@Copans-MacBook-Air.local>
2026-06-02 18:57:04 +0530

    husky test 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import AddTodo from './components/AddTodo'
 import TodoList from './components/TodoList'
 
 export default function App() {
+  var x = 10;
+    var x = 10;
+      var x = 10;
+        var x = 10;
+          var x = 10;
+          
+
   return (
     <div className="container">
       <h1>Redux Toolkit Todo Demo</h1>
